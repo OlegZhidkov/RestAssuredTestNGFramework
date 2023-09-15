c07d7cbc09483257ccab4998b88ade65dcae2594
Oleg Zhidkov <gzhidkov@gmail.com>
2023-09-15 12:16:47 +0300

    TokenManager update

--- src/test/java/com/spotify/oauth2/api/TokenManager.java
+++ src/test/java/com/spotify/oauth2/api/TokenManager.java
@@ -27,6 +27,7 @@ public class TokenManager {
                 System.out.println("Token is good to use");
             }
         } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException("Abort! Failed to get a token");
         }
 
